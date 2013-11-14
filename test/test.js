e8bf6dc172ca5a81954c01e30f820bc9eb819420
amhed <aherrera@gmail.com>
2013-11-13 22:24:20 -0400

    Adding test structure/example

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert"); //this uses require.js to load modules
+
+describe('Parcheesi Game', function() {
+	describe('#startGame()', function() {
+		it('should be able to start a new game', function() {
+			assert.true(false);
+		})
+	})
+})
