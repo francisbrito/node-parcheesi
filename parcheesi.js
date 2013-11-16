53f212e257a884d66f3fabe729277ff7e7e2c9aa
amhed <aherrera@gmail.com>
2013-11-16 09:56:07 -0400

    Minor refactorings
    * closes #4 - Added a better constructor
    * performed jslint on parcheesi.js

--- parcheesi.js
+++ parcheesi.js
@@ -1,7 +1,8 @@
-/*jslint browser: false, nomen: true, sloppy: true*/
+/*jslint browser: false, nomen: true, white: true*/
 /*global require, console, exports*/
 
 var Player = function (color) {
+    'use strict';
     var player = {
         'color': color,
         'pawns': new Array(4)
@@ -11,33 +12,39 @@ var Player = function (color) {
 };
 exports.Player = Player;
 
-//TODO: this should use the constructor javascript pattern 
 var ParcheesiGame = function (numberOfPlayers) {
-    var i, 
+    'use strict';
+
+    //Checks that object is always constructed using 'new'
+    if (!(this instanceof ParcheesiGame)) {
+        return new ParcheesiGame(numberOfPlayers);
+    }
+
+    var i,
         realNumberOfPlayers = numberOfPlayers || 2,
-        colors = ['red', 'blue', 'yellow', 'green'],
+        colors              = ['red', 'blue', 'yellow', 'green'],        
         
-        randomize = function(min, max){
+        randomize = function (min, max) {
             // http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
             return Math.floor(Math.random() * (max - min + 1) + min);
         },
-
+        
         game = {
             spaces: new Array(64),
             players: [],
-            throwDices: function(){
-                return [randomize(1,6),randomize(1,6)];
+            throwDices: function() {
+                return [randomize(1, 6), randomize(1, 6)];
             }
         };
     
-    if (realNumberOfPlayers > 4){
+    if (realNumberOfPlayers > 4) {
         throw new Error('Wrong number of players');
     }
 
-    for (i = 0; i < (realNumberOfPlayers); i += 1) {
+    for (i = 0; i < realNumberOfPlayers; i += 1) {
         game.players.push(new Player(colors[i]));
     }
 
-	return game;
+    return game;
 };
 exports.ParcheesiGame = ParcheesiGame;
